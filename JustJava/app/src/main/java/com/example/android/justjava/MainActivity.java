9a71a109b9c3d93434304c5cf01ba6ded8a5ec3c
aashish22bansal <aashish22bansal@gmail.com>
2021-07-30 00:08:08 +0530

    Started the Project Just Java
    Just Java is a basic app which can be used to give orders and calculate and display the price.

---
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.android.justjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+//import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
+
+public class MainActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+    * This method is called when the order button is clicked.
+    */
+    public void submitOrders(View view){
+        int quantity = 2, priceOfCoffee=5;
+        display(quantity);
+        displayPrice(quantity * priceOfCoffee);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    private void display(int number){
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText(""+number);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void displayPrice(int number){
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+}
